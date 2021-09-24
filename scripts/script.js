3938a379e475483ea2c4f73a8bcaac234a959fc0
VidarSmits <90198428+VidarSmits@users.noreply.github.com>
2021-09-24 15:14:25 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,16 @@
 
 
 var deButton = document.querySelector("button");
+var span1 = document.querySelector("button span:nth-of-type(1)")
+var span2 = document.querySelector("button span:nth-of-type(3)")
 var deNav = document.querySelector("nav:first-of-type")
 
 deButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   deNav.classList.toggle("toonMenu");
+  span1.classList.toggle("toonMenu");
+  span2.classList.toggle("toonMenu");
 }
 
 
